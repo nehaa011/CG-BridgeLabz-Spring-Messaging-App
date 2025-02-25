e1d742513b513ad95f340de4d0de3cd663f19857
Neha Gour <nehagour46420@gmail.com>
2025-02-25 14:54:21 +0530

    [Neha Gour] Add UC-2 Task

--- src/main/java/com/Spring/HelloProject/HelloController.java
+++ src/main/java/com/Spring/HelloProject/HelloController.java
@@ -1,18 +1,25 @@
 package com.Spring.HelloProject;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
     // UC1: Handle GET Request to return a Hello message
-    @GetMapping
-    public String sayHello(){
-        return "Hello from BridgeLabz";
-    }
+    //@GetMapping
+    // public String sayHello(){
+    //     return "Hello from BridgeLabz";
+    // }
+
+
+     //UC2: GET request with a query parameter
+     @GetMapping
+     public String sayHello(@RequestParam String name){
+        return "Hello, " + name + " from BridgeLabz";
+     }
+
+
 
 }
